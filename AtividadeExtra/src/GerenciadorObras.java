a1c44885c1240179456b269be35bb7f6d30490a0
ZFidelis <isaaczapo@hotmail.com>
2024-05-21 22:31:39 -0300

    Created methods to update, delete and search itens

--- AtividadeExtra/src/GerenciadorObras.java
+++ AtividadeExtra/src/GerenciadorObras.java
@@ -27,8 +27,69 @@ public class GerenciadorObras {
             throw new Exception("\nNenhuma Obra foi cadastrada");
         }
 
-
         return listaObras;
     } 
 
+    public static Obra buscarObra(String titulo) throws Exception {
+        ArrayList<Obra> listaObras = listarObras();
+
+        for (Obra tempObra : listaObras) {
+            if (tempObra.getTitulo().equals(titulo)) {
+                return tempObra;
+            }
+        }
+
+        throw new Exception("\nA Obra '" + titulo + "' não foi localizada!");
+    }
+
+
+    public static void excluirObra(String titulo) throws Exception, IOException {
+        ArrayList<Obra> listaObras = listarObras();
+
+        boolean encontrou = false;
+        for (Obra tempObra : listaObras) {
+            if (tempObra.getTitulo().equals(titulo)) {
+                listaObras.remove(tempObra);
+                encontrou = true;
+                break;
+            }
+        }
+
+        if (!encontrou) {
+            throw new Exception("A Obra '" + titulo + "' não foi localizada");
+        }
+
+        
+        try (FileWriter fw = new FileWriter(ARQUIVO); BufferedWriter bw = new BufferedWriter(fw)) {
+            for (Obra tempObra : listaObras) {
+                bw.write(tempObra + "\n");
+            }
+        }
+    }
+
+    public static void atualizarObra(String titulo) throws Exception, IOException {
+        ArrayList<Obra> listaObras = listarObras();
+
+        int pos = 0;
+        boolean encontrou = false;
+        for (Obra tempObra : listaObras) {
+            if (tempObra.getTitulo().equals(titulo)) {
+                pos = listaObras.indexOf(tempObra);
+                encontrou = true;
+                break;
+            }
+        }
+        
+        if (encontrou){
+            listaObras.get(pos).setTitulo("Jeref");
+            try (FileWriter fw = new FileWriter(ARQUIVO); BufferedWriter bw = new BufferedWriter(fw)) {
+                for (Obra tempObra : listaObras) {
+                    bw.write(tempObra + "\n");
+                }
+            }
+        }
+        else {
+            throw new Exception("A Obra '" + titulo + "' não foi localizada");
+        }
+    }
 }
